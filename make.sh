853891b12c93b9ad6ccb49dbdd63f42d409f8760
xungeng <xungeng@gmail.com>
2017-11-14 00:22:36 +0800

    make

--- make.sh
+++ make.sh
@@ -19,13 +19,13 @@ flags=(cycc -- -miphoneos-version-min=2.0 -arch armv6)
 else
 
 out=out
-sudo xcode-select --switch /Applications/Xcode-5.1.1.app
+sudo xcode-select --switch /Applications/Xcode.app
 
 if which xcrun &>/dev/null; then
     flags=(xcrun -sdk macosx g++)
     flags+=(-mmacosx-version-min=10.4)
 
-    for arch in i386 x86_64; do
+    for arch in x86_64; do
         flags+=(-arch "${arch}")
     done
 else
@@ -36,7 +36,7 @@ fi
 
 sdk=$(xcodebuild -sdk iphoneos -version Path)
 
-flags+=(-I.)
+flags+=(-I. -I/opt/local/include)
 flags+=(-I"${sdk}"/usr/include/libxml2)
 flags+=(-Ilibplist/include)
 flags+=(-Ilibplist/libcnary/include)
@@ -58,7 +58,7 @@ done
 set -x
 
 "${flags[@]}" -c -std=c++11 -o "${out}"/ldid.o ldid.cpp
-"${flags[@]}" -o "${out}"/ldid "${out}"/ldid.o "${os[@]}" -x c lookup2.c -lxml2 -framework Security -lcrypto
+"${flags[@]}" -o "${out}"/ldid "${out}"/ldid.o "${os[@]}" -x c lookup2.c -lxml2 -framework Security -L/opt/local/lib -lcrypto
 
 if ! "${ios}"; then
     ln -sf out/ldid .
